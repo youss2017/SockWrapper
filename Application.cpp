d75c06c45645f41843d30f3083e120935afdbd87
youss2017 <samwelyoussef123.2@gmail.com>
2022-07-19 16:07:34 -0500

    Fix Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -5,30 +5,29 @@
 namespace Application
 {
 	using namespace std;
-	using namespace SockWrapper;
 
 	int Start(int argc, char** argv)
 	{
-		SockWrapper::Startup();
+		sw::Startup();
 
-		Socket spam(SocketType::UDP);
-		spam.Bind(443, SocketInterface::Any);
+		sw::Socket spam(sw::SocketType::UDP);
+		spam.Bind(443, sw::SocketInterface::Any);
 		while (true)
 		{
 			char randomData[1024];
 			// Open Task Manager to see Network Traffic Spike.
 			strcpy(randomData, "SPAM UDP Packet! You can see this in wireshark! Broadcast address based on adapter.");
-			spam.SendTo(randomData, 1024, Endpoint::GetEndPointBroadcast(443));
+			spam.SendTo(randomData, 1024, sw::Endpoint::GetEndPointBroadcast(443));
 		}
 
-		Socket server(SocketType::TCP);
-		server.Bind(1500, SocketInterface::Any).Listen(10);
+		sw::Socket server(sw::SocketType::TCP);
+		server.Bind(1500, sw::SocketInterface::Any).Listen(10);
 
 		while (true)
 		{
 			try
 			{
-				Socket client = server.Accept();
+				sw::Socket client = server.Accept();
 				client.SetBlockingMode(false);
 			}
 			catch (std::exception& e)
@@ -37,7 +36,7 @@ namespace Application
 			}
 		}
 
-		SockWrapper::CleanUp();
+		sw::CleanUp();
 		return 0;
 	}
 
